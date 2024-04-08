96367212d09abf3ca8bbbadb22c339b1177778eb
Prabal Jain <prabalj29@gmail.com>
2024-04-08 23:06:43 +0530

    First Node.js Commit

---
+++ server.js
@@ -0,0 +1,68 @@
+const path=require("path");
+const http=require('http');
+const express =require("express");
+const socketio=require("socket.io");
+const formatMessage=require('./utils/messages');
+const {userJoin,getCurrentUser,userLeave,getRoomUsers}=require('./utils/users');
+
+
+const app = express();
+
+const server=http.createServer(app);
+const io=socketio(server);
+
+app.use(express.static(path.join(__dirname,'public')));
+
+const botName='ChatBoard Bot';
+
+io.on('connection',(socket) =>{
+
+    socket.on('joinRoom',({username,room})=>{
+        const user=userJoin(socket.id,username,room);
+
+        socket.join(user.room);
+
+
+        socket.emit('message',formatMessage(botName,"Welcome to chatBoard"));
+
+        socket.broadcast.to(user.room).emit('message',formatMessage(botName,`${user.username} user has joined the chat`));
+    
+        io.to(user.room).emit('roomUsers',{
+            room: user.room,
+            users: getRoomUsers(user.room)
+        });
+
+
+    });
+    // console.log('New Ws Connection....'); 
+
+
+    socket.on('chatMessage',(msg)=>{
+      const user=getCurrentUser(socket.id);
+
+       io.to(user.room).emit('message',formatMessage(user.username,msg));
+   });
+
+    socket.on('disconnect',()=>{
+const user=userLeave(socket.id);
+
+if(user){
+    io.emit('message',formatMessage(botName,`${user.username} has left the chat`));
+    
+    io.to(user.room).emit('roomUsers',{
+    room: user.room,
+    users: getRoomUsers(user.room)
+});
+};
+
+
+
+    });
+
+});
+
+const PORT=3000||process.env.PORT;
+
+server.listen(PORT,()=>
+    console.log(`Server running on port ${PORT} `)
+);
